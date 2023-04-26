c2d4e4f1437d8109a005064dda870a1605573ab0
Rony Pablo <ronyalexis0@gmail.com>
2023-04-26 08:41:51 -0700

    staging

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
-    'http://localhost:3000',
     'https://taquiza-steet.onrender.com',
     'https://www.taquiza-steet.onrender.com'
 ]
+//we are deleting 'http://localhost:3000', when testing locally maybe add it again
 
 module.exports = allowedOrigins;
--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,7 +2,7 @@ const allowedOrigins = require('./allowedOrigins');
 
 const corsOptions = {
     origin: (origin, callback) => {
-        if(allowedOrigins.indexOf(origin) !== -1 || !origin){
+        if(allowedOrigins.indexOf(origin) !== -1){
             //if we want to use postman to test routes add "|| !origin" in if statement 
             callback(null, true)
         }else{
